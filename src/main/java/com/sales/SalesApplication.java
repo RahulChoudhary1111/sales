0e4f859fd7d3759b25aba3941921db54fd721a56
JAVA <rahul@dil.in>
2023-11-01 17:17:25 +0530

    rahul changed application file

--- src/main/java/com/sales/SalesApplication.java
+++ src/main/java/com/sales/SalesApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SalesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SalesApplication.class, args);
+		System.out.println("Hello folks");
+		System.out.println("whats up!");
 	}
 
 }
