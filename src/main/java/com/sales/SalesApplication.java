079ef51f3503a557a0ff74dd359900b293ed0ef7
SAGAR <skbnsl20@gmail.com>
2023-11-01 18:09:09 +0530

    som change sagar

--- src/main/java/com/sales/SalesApplication.java
+++ src/main/java/com/sales/SalesApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SalesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SalesApplication.class, args);
-		System.out.println("Hello rahul");
-		System.out.println("Hello sager");System.out.println("Hello sager");
+		System.out.println("Hello folks");
+		System.out.println("whats up!");
+		System.out.println("sagar");
 	}
 
 }
