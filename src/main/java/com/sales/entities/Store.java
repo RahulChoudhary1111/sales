6339004fae6b17bfd371f7d2079e203797c9c8bd
JAVA <rahul@dil.in>
2023-11-03 10:30:06 +0530

    rahul changed application file

--- src/main/java/com/sales/entities/Store.java
+++ src/main/java/com/sales/entities/Store.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -15,6 +16,7 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "store")
+@Where(clause = "is_deleted != 'Y'")
 public class Store implements Serializable {
     
     @Id
